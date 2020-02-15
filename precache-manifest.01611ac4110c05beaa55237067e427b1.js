1add03ea0d6c6eb75d0eb359e0a1b7f0f0763fd5
Tolulope Olaniyan <tolulopeolaniyan@Tolulopes-MacBook-Pro.local>
2020-02-15 13:37:36 +0100

    Updates

---
+++ precache-manifest.01611ac4110c05beaa55237067e427b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3a26d454bf7648319a7e1d2ce2cf304",
+    "url": "/scream-social-app-react/index.html"
+  },
+  {
+    "revision": "ad7aafeeee74d3b7d2a3",
+    "url": "/scream-social-app-react/static/css/main.8845abdb.chunk.css"
+  },
+  {
+    "revision": "b9ef48af0c0ec061e946",
+    "url": "/scream-social-app-react/static/js/2.4745d77b.chunk.js"
+  },
+  {
+    "revision": "6f5f1c4a5f7f6e382b8aeca1b4ac8d96",
+    "url": "/scream-social-app-react/static/js/2.4745d77b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad7aafeeee74d3b7d2a3",
+    "url": "/scream-social-app-react/static/js/main.896e1e30.chunk.js"
+  },
+  {
+    "revision": "eae717485bca71008307",
+    "url": "/scream-social-app-react/static/js/runtime-main.bb3301fd.js"
+  }
+]);
